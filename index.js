8d8ba9e465700220d316ec89468cf6e9398857ea
Shreyansh Srivastava <srivastavashreyansh30@gmail.com>
2024-04-10 21:17:29 -0500

    Add different color pens

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     const container = document.querySelector('.container');
+    const blackBtn = document.querySelector('#black-btn');
+    const greyBtn = document.querySelector('#gray-btn');
     let isDrawing = false;
     let rows = 16;
     let columns = 16;
 
     function createGrid(rows, columns) {
+        const squareSize = Math.floor(500 / rows);
         container.innerHTML = '';
         for (let i = 0; i < rows; i++) {
             const row = document.createElement('div');
@@ -15,6 +17,8 @@ document.addEventListener("DOMContentLoaded", function () {
             for (let j = 0; j < columns; j++) {
                 const sqr = document.createElement('div');
                 sqr.classList.add('squares');
+                sqr.style.width = `${squareSize - 2}px`;
+                sqr.style.height = `${squareSize - 2}px`;
                 row.appendChild(sqr);
             }
         }
@@ -22,36 +26,73 @@ document.addEventListener("DOMContentLoaded", function () {
 
     createGrid(rows, columns);
 
-    const pixels = document.querySelectorAll('.squares');
-    pixels.forEach((pixel) => {
+    function blackPen() {
 
-        pixel.addEventListener('mousedown', function () {
-            isDrawing = true;
-            pixel.classList.add('hovered');
-        });
+        const pixels = document.querySelectorAll('.squares');
+        blackBtn.addEventListener('click', () => {
+            console.log("click");
+            pixels.forEach((pixel) => {
 
-        pixel.addEventListener('mouseenter', function () {
-            if (isDrawing) {
-                pixel.classList.add('hovered');
-            }
-        });
+                pixel.addEventListener('mousedown', function () {
+                    isDrawing = true;
+                    pixel.classList.add('hovered');
+                });
 
-        pixel.addEventListener('mouseup', function () {
-            isDrawing = false;
+                pixel.addEventListener('mouseenter', function () {
+                    if (isDrawing) {
+                        pixel.classList.add('hovered');
+                    }
+                });
+
+                pixel.addEventListener('mouseup', function () {
+                    isDrawing = false;
+                });
+            });
         });
-    });
+    }
+
+    blackPen();
+
+    function grayPen() {
+
+        const pixels = document.querySelectorAll('.squares');
+        greyBtn.addEventListener('click', () => {
+            console.log("click");
+            pixels.forEach((pixel) => {
+
+                pixel.addEventListener('mousedown', function () {
+                    isDrawing = true;
+                    pixel.classList.add('hovered');
+                });
+
+                pixel.addEventListener('mouseenter', function () {
+                    if (isDrawing) {
+                        let Rnum = Math.floor(Math.random() * 255);
+                        pixel.style.backgroundColor = `rgb(${Rnum}, ${Rnum}, ${Rnum})`;
+                    }
+                });
+
+                pixel.addEventListener('mouseup', function () {
+                    isDrawing = false;
+                });
+            });
+        });
+    }
+
+    grayPen();
+
 
     const button = document.querySelector('.button');
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         const userInput = prompt("Enter the number of square per side for the new grid");
         const newSize = parseInt(userInput);
 
-        if(!isNaN(newSize) && newSize > 0 && newSize < 100){
+        if (!isNaN(newSize) && newSize > 0 && newSize < 100) {
             rows = columns = newSize;
             createGrid(rows, columns);
         }
-        else{
+        else {
             alert("Enter a valid number");
         }
     });
